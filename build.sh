657e3bedd4fac7e18f25823ab62691de80199372
Joaquín Nicolás Carena <89748758+joacocarena@users.noreply.github.com>
2023-02-09 15:57:35 -0300

    update2

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 set -o errexit
 
 #poetry install
-#pip install -r requirements.txt
+pip install -r requirements.txt
+pip install gunicorn
 
 python manage.py collectstatic --no-input
 python manage.py migrate
