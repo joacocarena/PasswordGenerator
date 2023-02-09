aa9d662835902962981cd8abfa7b8e2b07e6d9bc
Joaquín Nicolás Carena <89748758+joacocarena@users.noreply.github.com>
2023-02-09 16:26:15 -0300

    update3

--- pass_generator/settings.py
+++ pass_generator/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-r*kbf0e@xd#kiafdk^fv@mb-b1&qat76nn6r8xja*hhh#k9qb=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
