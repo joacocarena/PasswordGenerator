568edcaa60e27c19a8664a5dacdafaee45d7e575
Joaquín Nicolás Carena <89748758+joacocarena@users.noreply.github.com>
2023-02-10 20:54:44 -0300

    update6

--- pass_generator/settings.py
+++ pass_generator/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'pass_generator.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default= 'postgresql://postgres:postgres@localhost:8000/postgres',
+        default= 'postgresql://postgres:postgres@localhost:5432/postgres',
         conn_max_age=600,
     )
 }
